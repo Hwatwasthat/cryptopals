c9d71d77f519a0aa4ac6d76f52e00df6a472bfda
chris <chris.j.bury@googlemail.com>
2018-11-12 23:48:51 +0000

    Implemented chiSquared Frequency checking Placed testers under interface for future experimentation

--- challenges/set1/challenge6.go
+++ challenges/set1/challenge6.go
@@ -9,10 +9,9 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"sync"
 )
 
-func Challenge6(filename string) {
+func Challenge6(filename string, test english.Tester) {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalln(err)
@@ -33,26 +32,13 @@ func Challenge6(filename string) {
 
 	transposed := utilities.Transpose(decodedBytes, keysize[0]) // dangerous assumption that keysize[0] is accurate
 
-	wg := &sync.WaitGroup{}
-	c := make(chan english.GuessstringByte)
-	for _, s := range transposed {
-		wg.Add(1)
-		go english.ConcurrentGuessString(c, wg, s) // concurrent implementation of MostEnglish
-	}
-
-	go monitorWG(wg, c)
-
 	var guesses []byte
-	for ret := range c {
-		guesses = append(guesses, ret.XorByte)
+	for _, s := range transposed {
+		ret, _ := test.GuessString(s)
+		guesses = append(guesses, ret)
 	}
 
 	result := xor.RepKey(decodedBytes, guesses)
 	fmt.Println(string(result))
 
-}
-
-func monitorWG(wg *sync.WaitGroup, c chan english.GuessstringByte) {
-	wg.Wait()
-	close(c)
 }
